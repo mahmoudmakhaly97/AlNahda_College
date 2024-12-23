182307aba38fd0fbdbe8715f59c0451787caf94f
mahmoud makhaly <mahmoud.abdelfatah@5d-agency.com>
2024-12-23 10:35:03 +0200

    initialize reposetery

---
+++ js/main.js
@@ -0,0 +1,77 @@
+// latest offers carousel 
+$('.banner-carousel').owlCarousel({
+    loop:true,
+    rtl:true,
+
+    autoplay:true,
+
+    navText: ["<i class='fas fa-chevron-right'></i>","<i class='fas fa-chevron-left'></i>"], 
+
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+ 
+// academic programs carousel
+$('.academic-programs-carousel').owlCarousel({
+    loop:true,
+    rtl:true,
+
+    autoplay:true,
+
+    navText: [" <img src='../images/academic-programs/prev.svg' class='next-icon' alt=''>"," <img class='prev-icon' src='../images/academic-programs/next.svg' alt=''>"],
+
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+ 
+// academic programs carousel
+$('.events-carousel').owlCarousel({
+    loop:true,
+    rtl:true,
+
+    autoplay:true,
+
+    navText: ["<i class='fas fa-chevron-right'></i>","<i class='fas fa-chevron-left'></i>"], 
+
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+// start data aos animation
+   window.addEventListener('load', () => {
+      AOS.init({
+  duration: 1000, 
+  once: true,    
+  offset: 120,   
+});
+  });
+ // end data aos animation
