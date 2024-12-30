39b995de2f5effa074a57d646de228bcaf58e389
mahmoud makhaly <mahmoud.abdelfatah@5d-agency.com>
2024-12-30 18:15:06 +0200

    -add floating navbar

--- js/main.js
+++ js/main.js
@@ -132,6 +132,57 @@ window.addEventListener("scroll", function () {
   }
 });
 
+        	// scrolled navbar 
+var scrollWindow = function () {
+  
+    $(window).scroll(function () {
+    var navbar = document.querySelector(".navbar");
+			var $w = $(this),
+        st = $w.scrollTop(),
+          
+
+        
+					navbar = $('.ftco_navbar'),
+					sd = $('.js-scroll-wrap');
+ 			if (st > 80) {
+				if ( !navbar.hasClass('scrolled') ) {
+                    navbar.addClass('scrolled');	
+                  
+              //  document.getElementById("logo").src = "../images/home/navbar/colored-logo.svg";
+
+				}
+                
+			} 
+			if (st < 80) {
+				if ( navbar.hasClass('scrolled') ) {
+                    navbar.removeClass('scrolled sleep');
+                                  //  document.getElementById("logo").src = "../images/home/navbar/logo.svg";
+
+				}
+			} 
+			if ( st > 80 ) {
+				if ( !navbar.hasClass('awake') ) {
+					navbar.addClass('awake');	
+				}
+				
+				if(sd.length > 0) {
+					sd.addClass('sleep');
+				}
+			}
+			if ( st < 105 ) {
+				if ( navbar.hasClass('awake') ) {
+					navbar.removeClass('awake');
+					navbar.addClass('sleep');
+				}
+				if(sd.length > 0) {
+					sd.removeClass('sleep');
+				}
+			}
+		});
+	};
+scrollWindow();
+  
+
 // end navbar scroll logic
 // start cursor shape logic
 const cursor = document.querySelector(".custom-cursor");
